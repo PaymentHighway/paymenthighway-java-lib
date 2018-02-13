c46a36d172b16126e74dc11e7e59d210940ebed9
Pauli Kostamo <pauli.kostamo@solinor.com>
2018-02-13 13:31:12 +0200

    Pivo payment parameters, Fix one 2017 expired test card

---
+++ src/main/java/io/paymenthighway/formBuilders/PivoParametersBuilder.java
@@ -0,0 +1,69 @@
+package io.paymenthighway.formBuilders;
+
+public class PivoParametersBuilder
+    extends GenericPaymentParametersBuilder<PivoParametersBuilder>
+    implements PivoParametersInterface {
+
+    public PivoParametersBuilder(
+        String method,
+        String signatureKeyId,
+        String signatureSecret,
+        String account,
+        String merchant,
+        String baseUrl,
+        String successUrl,
+        String failureUrl,
+        String cancelUrl,
+        Long amount,
+        String orderId,
+        String description
+    ) {
+        super(
+            method,
+            signatureKeyId,
+            signatureSecret,
+            account,
+            merchant,
+            baseUrl,
+            successUrl,
+            failureUrl,
+            cancelUrl,
+            amount.toString(),
+            "EUR",
+            orderId,
+            description
+        );
+        serviceUri = "/form/view/pivo";
+    }
+
+    /**
+     * @param phoneNumber Customer phone number with country code e.q. +358449876543. Makes it easier for the customer to identify himself.
+     * @return PivoParametersInterface
+     */
+    public PivoParametersBuilder phoneNumber(String phoneNumber) {
+        addNameValuePair(FormBuilderConstants.SPH_PHONE_NUMBER, phoneNumber);
+        return this;
+    }
+
+    /**
+     * Reference number
+     *
+     * @param referenceNumber Reference number
+     * @return PivoParametersInterface
+     */
+    public PivoParametersBuilder referenceNumber(String referenceNumber) {
+        addNameValuePair(FormBuilderConstants.SPH_REFERENCE, referenceNumber);
+        return this;
+    }
+
+    /**
+     * When used, Pivo tries to open application with this url
+     *
+     * @param appUrl App url
+     * @return Form builder
+     */
+    public PivoParametersInterface appUrl(String appUrl) {
+        addNameValuePair(FormBuilderConstants.SPH_APP_URL, appUrl);
+        return this;
+    }
+}
---
+++ src/main/java/io/paymenthighway/formBuilders/PivoParametersInterface.java
@@ -0,0 +1,82 @@
+package io.paymenthighway.formBuilders;
+
+import io.paymenthighway.FormContainer;
+
+public interface PivoParametersInterface {
+    /**
+     * Exit from iframe after a result
+     *
+     * @param exitIframeOnResult Exit from iframe after a result.
+     * @return Form builder
+     */
+    PivoParametersInterface exitIframeOnResult(Boolean exitIframeOnResult);
+    
+    /**
+     * @param phoneNumber Customer phone number with country code e.q. +358449876543. Makes it easier for the customer to identify himself.
+     * @return Form builder
+     */
+    PivoParametersInterface phoneNumber(String phoneNumber);
+
+    /**
+     * Set PaymentHighway form language
+     *
+     * @param language Two character language code e.q. FI or EN. If omitted, default language from user's browser's settings is used.
+     * @return Form builder
+     */
+    PivoParametersInterface language(String language);
+
+    /**
+     * The URL the PH server makes request after the transaction is handled. The payment itself may still be rejected.
+     *
+     * @param successUrl Webhook url to call when request is successfully handled
+     * @return Form builder
+     */
+    PivoParametersInterface webhookSuccessUrl(String successUrl);
+
+    /**
+     * The URL the PH server makes request after a failure such as an authentication or connectivity error.
+     *
+     * @param failureUrl Webhook url to call when request failed
+     * @return Form builder
+     */
+    PivoParametersInterface webhookFailureUrl(String failureUrl);
+
+    /**
+     * The URL the PH server makes request after cancelling the transaction (clicking on the cancel button).
+     *
+     * @param cancelUrl Webhook url to call when user cancels request
+     * @return Form builder
+     */
+    PivoParametersInterface webhookCancelUrl(String cancelUrl);
+
+    /**
+     * Delay for webhook in seconds. Between 0-900
+     *
+     * @param delay Webhook triggering delay in seconds
+     * @return Form builder
+     */
+    PivoParametersInterface webhookDelay(Integer delay);
+
+    /**
+     * Reference number
+     *
+     * @param referenceNumber Reference number
+     * @return Form builder
+     */
+    PivoParametersInterface referenceNumber(String referenceNumber);
+
+    /**
+     * When used, Pivo tries to open application with this url
+     *
+     * @param appUrl App url
+     * @return Form builder
+     */
+    PivoParametersInterface appUrl(String appUrl);
+
+    /**
+     * Builds form parameters
+     *
+     * @return FormContainer
+     */
+    FormContainer build();
+}
