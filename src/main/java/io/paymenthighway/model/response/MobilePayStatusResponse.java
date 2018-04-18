209116ec5c36c5eea1d0485f70922ce649b87948
jarikujansuu <jari.kujansuu@solinor.com>
2018-04-18 10:42:32 +0300

    Use snake_case for json keys.

--- src/main/java/io/paymenthighway/model/response/MobilePayStatusResponse.java
+++ src/main/java/io/paymenthighway/model/response/MobilePayStatusResponse.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class MobilePayStatusResponse extends Response {
   @JsonProperty("status") private String status;
-  @JsonProperty("transaction-id") private String transactionId;
-  @JsonProperty("valid-until") private String validUntil;
+  @JsonProperty("transaction_id") private String transactionId;
+  @JsonProperty("valid_until") private String validUntil;
 
   public String getStatus() {
     return status;
