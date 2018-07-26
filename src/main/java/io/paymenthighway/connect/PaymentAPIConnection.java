628923503fe7b4cb63b188277f9de81094ca3d91
Terho Siikanen <support@paymenthighway.fi>
2018-07-26 10:31:20 +0300

    Include the api version in the PaymentAPI user-agent

--- src/main/java/io/paymenthighway/connect/PaymentAPIConnection.java
+++ src/main/java/io/paymenthighway/connect/PaymentAPIConnection.java
@@ -35,7 +35,7 @@ import java.util.UUID;
 public class PaymentAPIConnection implements Closeable {
 
   /* Payment API headers */
-  private static final String USER_AGENT = "PaymentHighway Java Lib";
+  private static final String USER_AGENT = "PaymentHighway Java Lib/" + Constants.API_VERSION;
   private static final String METHOD_POST = "POST";
   private static final String METHOD_GET = "GET";
   private static final String SPH_API_VERSION = Constants.API_VERSION;
