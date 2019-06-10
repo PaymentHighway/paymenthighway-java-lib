aadbb260c89735b9273806246e77a035060ea7e0
Pauli Kostamo <pauli.kostamo@paymenthighway.fi>
2019-06-10 15:03:05 +0300

    transaction id to response

--- src/main/java/io/paymenthighway/model/response/PivoInitResponse.java
+++ src/main/java/io/paymenthighway/model/response/PivoInitResponse.java
@@ -3,9 +3,14 @@ package io.paymenthighway.model.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class PivoInitResponse extends Response {
+    @JsonProperty("transaction_id") private String transactionId;
     @JsonProperty("uri") private String uri;
     @JsonProperty("valid_until") private String validUntil;
     @JsonProperty("result") private Result result;
+
+    public String getTransactionId() {
+        return transactionId;
+    }
 
     public String getUri() {
         return uri;
